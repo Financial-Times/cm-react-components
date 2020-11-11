b67e2db1359c0da07dadd7f6247f92d88186276d
Yordan Atanasov <yordan.atanasov@ft.com>
2020-11-11 12:04:45 +0200

    UPPSF-1890 added another button for test purposes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './styles/index.scss';
 
 import Button from './components/Button/Button.js';
+import AnotherButton from './components/AnotherButton/AnotherButton.js';
 
 export default Button;
+export default AnotherButton;
